4ac12e2518cc00da30c710ed96622ba1754107e6
Fill <bluizfillypper@gmail.com>
2024-08-02 22:10:07 -0300

    thrid commit

---
+++ conhecendo_a_linguagem/convertendo_tipos.py
@@ -0,0 +1,10 @@
+print(int(1.9))
+
+print(float("10.10"))
+
+print(float(100))
+
+print(str(10.10))
+
+print(100 / 2)
+print(100 // 2)
---
+++ conhecendo_a_linguagem/variaveis_constantes.py
@@ -0,0 +1,13 @@
+nome = "Luiz"
+idade = 25
+
+nome, idade = "Giovanna" , 27
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+
+BRIZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRIZILIAN_STATES)

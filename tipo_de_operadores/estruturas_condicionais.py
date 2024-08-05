cdb81ffaddabbc143a93cd2b982f1e6dd5a62719
Fillypper <99841495+Fillypper@users.noreply.github.com>
2024-08-05 12:00:19 -0300

    Create estruturas_condicionais.py

---
+++ tipo_de_operadores/estruturas_condicionais.py
@@ -0,0 +1,14 @@
+MAIOR_IDADE = 18
+
+idade = int(input("Informe sua idade: "))
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH.")
+
+if idade <= MAIOR_IDADE:
+    print("Menor de idade, não pode tirar a CNH")
+
+if idade >= MAIOR_IDADE:
+    print("Maior de idade, pode tirar a CNH.")
+else:
+    print("Menor de idade, não pode tirar a CNH")

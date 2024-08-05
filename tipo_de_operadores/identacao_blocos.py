7a79d9f8ddf8e187c3445395d9b2e4d25418c5aa
Fillypper <99841495+Fillypper@users.noreply.github.com>
2024-08-05 11:41:33 -0300

    identacao_blocos
    Learning how to ident the code with functions and parameters

---
+++ tipo_de_operadores/identacao_blocos.py
@@ -0,0 +1,15 @@
+def sacar(valor: float):
+    saldo = 500
+
+    if saldo >= valor:
+        print("valor sacado!")
+        print("retire o seu dinheiro na boa do caixa.")
+    print("Obrigado por ser nosso cliente, tenha um bom dia!")
+
+
+def depositar(valor):
+    saldo = 500
+    saldo+= valor
+    
+
+sacar(1000)

becfb0ac872fa0551713e906589347c8218da6a6
Fill <bluizfillypper@gmail.com>
2024-08-07 18:50:59 -0300

    adding 2 file for loopings

--- estruturas_condicionais_e_repetição/estrutura_repeticao_for.py
+++ estruturas_condicionais_e_repetição/estrutura_repeticao_for.py
@@ -1,6 +1,5 @@
 texto = input("Informe um text: ")
 VOGAIS = "AEIOU"
-opcao = -1
 
 for letra in texto:
     if letra.upper() in VOGAIS:
@@ -13,10 +12,3 @@ print("Executa no final do laço")
 for numero in range(0,51,5):
     print(numero, end=" ")
 
-while opcao != 0:
-    opcao = int(input("[1] para sacar \n[2]Extrato\n[0] Sair\n: "))
-
-    if opcao == 1:
-        print("Sacando")
-    elif opcao == 2:
-        print("Exibindo o extrato...")

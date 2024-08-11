26e3de370db45c5ed7d119645a7f11d211818518
Fill <bluizfillypper@gmail.com>
2024-08-10 22:01:41 -0300

    updating

---
+++ 01_Estrutura_de_dados/02_listas/listas_1.py
@@ -0,0 +1,15 @@
+
+# Matriz Bidimensional
+
+matriz = [
+    [1, "a", 2],
+    ["b", 3, 1],
+    [6, "c", 4],
+]
+
+# Enumerate
+
+carros = ["gol", "celta", "palio"]
+
+for indice, carro in enumerate(carros): #indice retornará a posição e o carro retornará o valor desta posição
+    print(f"{indice}: {carro}")
---
+++ 01_Estrutura_de_dados/02_listas/metodos_listas.py
@@ -0,0 +1,10 @@
+# [].append
+
+# lista = []
+
+# lista.append(1)
+# lista.append("Python")
+# lista.append([40, 30, 20])
+
+# print(lista)
+

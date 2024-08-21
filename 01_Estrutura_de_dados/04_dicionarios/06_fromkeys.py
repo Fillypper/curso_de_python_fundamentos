217e22820042d5722c5fa64191bb499b18cc2f30
Fillypper <bluizfillypper@gmail.com>
2024-08-21 13:23:42 -0300

    .

---
+++ 01_Estrutura_de_dados/04_dicionarios/06_fromkeys.py
@@ -0,0 +1,7 @@
+
+lista = dict.fromkeys(["nome", "telefone"])
+
+lista1 = dict.fromkeys(["nome", "telefone"], "vazio")
+
+print(lista)
+print(lista1)
---
+++ 01_Estrutura_de_dados/04_dicionarios/07_get.py
@@ -0,0 +1,12 @@
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme","telefone": "3333-2221"}
+}
+
+#contatos["chave"]
+
+print(contatos.get("chave"))
+
+print(contatos.get("chave", {}))
+
+print(contatos.get("guilherme@gmail.com", {}))
+

106bfe3b137c9d1765d18684125a8df37eee24b8
Fillypper <bluizfillypper@gmail.com>
2024-08-21 13:05:56 -0300

    Ajusting

---
+++ 01_Estrutura_de_dados/04_dicionarios/00_declarando.py
@@ -0,0 +1,7 @@
+pessoa = {"nome": "Guilherme", "idade" : 28}
+
+pessoa = dict(nome="Guilherme", idade=28)
+
+pessoa["telefone"] = "3333-1234" #adicionando chave e valor no dicionário já criado
+
+print(pessoa)

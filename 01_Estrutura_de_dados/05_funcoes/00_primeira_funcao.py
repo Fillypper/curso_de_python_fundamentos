5bc4a4f756048a6cc24294307c3c049d86ff170e
Fill <bluizfillypper@gmail.com>
2024-08-24 17:46:27 -0300

    .

---
+++ 01_Estrutura_de_dados/05_funcoes/00_primeira_funcao.py
@@ -0,0 +1,13 @@
+def primeria_funcao():
+    print("Olá mundo!")
+
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo {nome}!")
+
+def exibir_mensagem_3(nome="Anonimo"):
+    print(f"Seja bem vindo {nome}!")
+
+primeria_funcao()
+exibir_mensagem_2(nome="FIll")
+exibir_mensagem_3()
+exibir_mensagem_3(nome="Chappie")
